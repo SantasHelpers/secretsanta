d0106f67f6234d17ca5adb11a94a27a6959e6f7c
Joosang Lee <armlessarcher@gmail.com>
2016-12-07 18:46:38 -0800

    Delete node_modules, add bodyparser to server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
+var path = require('path');
+var bodyparser = require('body-parser');
+var port = port = process.env.PORT || 8000;
 
 var app = express();
-
-
-
-
-var port = port = process.env.PORT || 8000;
+app.use('/', express.static(path.join(__dirname + '/../client')));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 app.listen(port);
 console.log('listening on ', port);
