7d295ee6019b0be7149668c465480c9e98f6db82
Joosang Lee <armlessarcher@gmail.com>
2016-12-08 11:37:08 -0800

    Fix typo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyparser = require('body-parser');
-var aws = require('awslib');
+var aws = require('aws-lib');
 var port = port = process.env.PORT || 8000;
 
 var app = express();
