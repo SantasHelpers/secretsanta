c9ede3f34761a0fab0073ed7983024150282f287
witygass <witygass@indiana.edu>
2016-12-07 18:54:48 -0800

    finished first draft db schema

--- server/schemaInit.sql
+++ server/schemaInit.sql
@@ -1,27 +1,54 @@
+DROP DATABASE santasHelpers;
 CREATE DATABASE santasHelpers;
 
 USE santasHelpers;
 
 CREATE TABLE users (
-
+  user_id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50),
+  firstname VARCHAR(50),
+  lastname VARCHAR(50),
+  password VARCHAR(50),
+  PRIMARY KEY (user_id)
 );
 
 CREATE TABLE groups (
-
+  group_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50),
+  summary TEXT,
+  category VARCHAR(50),
+  PRIMARY KEY (group_id)
 );
 
 CREATE TABLE items (
-
+  item_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50),
+  imageURL VARCHAR(255),
+  price DECIMAL(10,2),
+  category VARCHAR(50),
+  PRIMARY KEY (item_id)
 );
 
 -- join tables
 
 CREATE TABLE users_groups (
-  users_groups_id
-  user_id
-  group_id
+  user_group_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  group_id INT NOT NULL,
+  PRIMARY KEY (user_group_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (group_id) REFERENCES groups(group_id)
+  ON DELETE CASCADE
 );
 
 CREATE TABLE users_items (
-  id
+  user_item_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  item_id INT NOT NULL,
+  PRIMARY KEY (user_item_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (item_id) REFERENCES items(item_id)
+  ON DELETE CASCADE
 );
