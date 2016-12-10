0277ef8be9ab2b2eae516c01017e30789cc84c0d
Phil Barow <pbarow@gmail.com>
2016-12-10 12:19:12 -0800

    store working

--- client/components/SantaStore.js
+++ client/components/SantaStore.js
@@ -6,7 +6,10 @@ class SantaStore {
 
   constructor () {
     extendObservable(this, {
-      users: ['Phil', 'Tyler', 'Joos', 'Chan'],
+      images: [ 'http://www.bodybuilding.com/images/2016/july/train-like-dwayne-the-rock-johnson-bigshot-v2-830x467.jpg', 
+                'http://static1.gamespot.com/uploads/original/1179/11799911/2877070-jason.jpg', 
+                'http://cdn-mf0.heartyhosting.com/sites/mensfitness.com/files/main-how-to-build-shoulders-like-vin-diesel.jpg',
+                'http://www.pwpix.net/wp/wp-content/uploads/2015/04/john-cena-28361725-620x400.jpg'],
       groups: ['HR50', 'Family', 'Friends']
     })
     // this.users = ;
