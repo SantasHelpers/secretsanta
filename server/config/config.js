f69e095abf3e37f68814f632f00ca4ec9e100cf3
Joosang Lee <armlessarcher@gmail.com>
2016-12-11 11:18:17 -0800

    Remove config.js from git

--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-var keyId = 'AKIAJC7KFZJ5ZPDSPIIQ';
-var secretKey = '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';
-var associateId = 'jslee-20';
-module.exports = { keyId: keyId, secretKey: secretKey, aId : associateId };
