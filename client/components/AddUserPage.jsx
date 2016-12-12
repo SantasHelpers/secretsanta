00b28901f31b5578a6ae313aaca0ac920fa7f1fd
Phil Barow <pbarow@gmail.com>
2016-12-11 20:38:56 -0800

    added link to basic add user page

---
+++ client/components/AddUserPage.jsx
@@ -0,0 +1,16 @@
+// AddUserPage.jsx
+
+import React from 'react';
+
+// not sure if need MOBX here,  will hold off
+
+export default class AddUserPage extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <h2>This is the Add User Page</h2>
+      </div>
+      );
+  }
+}
