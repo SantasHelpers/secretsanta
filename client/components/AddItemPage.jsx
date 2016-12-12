97ef389f9364cffd517a5fff2169a416c7b8fdd1
Phil Barow <pbarow@gmail.com>
2016-12-11 21:44:57 -0800

    contining to buid out interaction with the server

--- client/components/AddItemPage.jsx
+++ client/components/AddItemPage.jsx
@@ -1,16 +1,48 @@
 // AddItemPage.jsx
 
 import React from 'react';
+import axios from 'axios';
+import { observer } from 'mobx-react';
+import santaStore from './SantaStore';
 
 // not sure if need MOBX here,  will hold off
 
-export default class AddItemPage extends React.Component {
-  render() {
+var AddItemPage = observer(class AddItemPage extends React.Component {
+  
+   constructor (props) {
+    super (props);
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
 
+  onButtonClick (e) {
+    e.preventDefault();
+    
+    var data = {
+      keyword: this.refs.keyword.value,
+      username: santaStore.username
+    };
+
+    axios.post('/api/item', {
+        data: data
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  };
+
+  render() {
     return (
       <div>
         <h2>This is the Add Item Page</h2>
+        <h5>:</h5><input type="text" name="keyword" ref="keyword" size="50"/><br/>
+        <button name="button" onClick={this.onButtonClick.bind(this)}>Search for Gifts</button>
       </div>
       );
   }
-}
+})
+
+export default AddItemPage;
