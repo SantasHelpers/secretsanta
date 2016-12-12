8c442491ddce94501f3aa913d66eb0967459f159
Joosang Lee <armlessarcher@gmail.com>
2016-12-12 12:35:36 -0800

    Made some fake data

---
+++ fakeUserData.js
@@ -0,0 +1,183 @@
+var fakeUserData = [{
+  username: 'phil'
+  name: 'Phil',
+  groups: ['HR50'],
+  items: [{
+    name: 'VonShef Oval Slide Out Bamboo Cheese Board and 4 Piece Knife Set',
+    price: 44.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51zXBog4vWL._SL160_.jpg',
+    URL: 'https://www.amazon.com/VonShef-Slide-Bamboo-Cheese-Board/dp/B0189GEUKC%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB0189GEUKC',
+    category: 'Santa'
+  }, {
+    name: 'European Assortment - 5 Cheeses (8 oz Each)',
+    price: 37.8,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41qtB-7FYaL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Gourmet555-European-Assortment-Cheeses-Each/dp/B002CR5EW8%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB002CR5EW8',
+    category: 'Santa'
+  }, {
+    name: 'Claey\'s, Old Fashioned Hard Candy Lemon, 6 Ounce Bag',
+    price: 5.22,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51PK8RKgj%2BL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Claeys-Fashioned-Candy-Lemon-Ounce/dp/B0006O4OI4%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB0006O4OI4',
+    category: 'Elf'
+  }, {
+    name: 'True Lemon 2.85 Ounce',
+    price: 9.18,
+    imageURL: 'http://ecx.images-amazon.com/images/I/419lGzr%2B82L._SL160_.jpg',
+    URL: 'https://www.amazon.com/True-Citrus-Lemon-2-85-Ounce/dp/B001Z2TTMM%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB001Z2TTMM',
+    category: 'Elf'
+  }, {
+    name: 'Apple USB-C Digital AV Multiport Adapter',
+    price: 79,
+    imageURL: 'http://ecx.images-amazon.com/images/I/21zgj7PV6RL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Apple-USB-C-Digital-Multiport-Adapter/dp/B00VU2K10G%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00VU2K10G',
+    category: 'Santa'
+  }, {
+    name: 'National Public Seating BT3060 Steel Frame Rectangular Blow Molded Plastic Top Folding Table, 1000 lbs Capacity, 60" Length x 30" Width x 29-1/2" Height, Speckled Gray/Gray',
+    price: 57.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/31ZWtobz6hL._SL160_.jpg',
+    URL: 'https://www.amazon.com/National-BT3060-Rectangular-Capacity-Speckled/dp/B005T7OFFK%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB005T7OFFK',
+    category: 'Santa'
+  }, {
+    name: 'Lord of the Rings: The Return of the King (Extended Edition)',
+    price: 17.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/515Uq0651TL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Lord-Rings-Return-King-Extended/dp/B009ITL7UG%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB009ITL7UG',
+    category: 'Raindeer'
+  }, {
+    name: 'Lord of the Rings: The Fellowship of the Ring - Extended Edition',
+    price: 17.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51NaunyiSkL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Lord-Rings-Fellowship-Ring-Extended/dp/B009F1DJRQ%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB009F1DJRQ',
+    category: 'Raindeer'
+  }, {
+    name: 'The Lord of the Rings: 50th Anniversary, One Vol. Edition',
+    price: 20,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51eq24cRtRL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Lord-Rings-50th-Anniversary-Vol/dp/0618640150%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D0618640150',
+    category: 'Raindeer'
+  }],
+}, {
+  username: 'tyl'
+  name: 'Tyler',
+  groups: ['HR50'],
+  items: [{
+    name: 'Apple MacBook MLHE2LL/A 12-Inch Laptop with Retina Display (1.1GHz Dual Core Intel m3, 8GB RAM, 256GB HD, OS X), Gold NEWEST VERSION',
+    price: 1299,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51MYSMT-GwL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Apple-MacBook-MLHE2LL-12-Inch-Display/dp/B01EIUEZ1W%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB01EIUEZ1W',
+    category: 'Santa'
+  }, {
+    name: 'Apple 13 " MacBook Pro / MD101LL/A / 2.5GHz Intel Core i5, 4GB RAM, 500GB HDD',
+    price: 1099,
+    imageURL: 'http://ecx.images-amazon.com/images/I/510z0ixPJGL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Apple-MacBook-MD101LL-2-5GHz-Intel/dp/B008BEYEL8%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB008BEYEL8',
+    category: 'Santa'
+  }, {
+    name: 'Acer G226HQL 21.5-Inch Screen LED Monitor',
+    price: 100.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41vQQo6HpgL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Acer-G226HQL-21-5-Inch-Screen-Monitor/dp/B009POS0GS%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB009POS0GS',
+    category: 'Santa'
+  }, {
+    name: 'IAMS PROACTIVE HEALTH Adult MiniChunks Dry Dog Food 30 Pounds',
+    price: 39.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51qkxlhGQBL._SL160_.jpg',
+    URL: 'https://www.amazon.com/PROACTIVE-HEALTH-Adult-MiniChunks-Pounds/dp/B00BD741WM%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00BD741WM',
+    category: 'Santa'
+  }, {
+    name: 'Friskies Cat Food Chicken & Tuna Dinner, 5.5 oz',
+    price: 33.56,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41gpkwhO4HL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Friskies-Food-Chicken-Tuna-Dinner/dp/B000RYCCFU%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB000RYCCFU',
+    category: 'Santa'
+  }]
+}, {
+  username: 'cha'
+  name: 'Chan',
+  groups: ['HR50'],
+  items: [{
+    name: 'Nat and Jules Brown Tiger Plush Toy, Large',
+    price: 21,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51QHDCnlgYL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Nat-Jules-Brown-Tiger-Plush/dp/B007B5H6HC%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB007B5H6HC',
+    category: 'Raindeer'
+  }, {
+    name: 'Tiger Stainless Steel Vacuum Insulated Sports Bottle, 34-Ounce, Pink',
+    price: 16.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41QLIqGauEL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Tiger-Stainless-Vacuum-Insulated-34-Ounce/dp/B00BE9XL52%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00BE9XL52',
+    category: 'Raindeer'
+  }, {
+    name: 'BestOffice Ergonomic PU Leather High Back Office Chair, Black',
+    price: 76.88,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41iySD1yuPL._SL160_.jpg',
+    URL: 'https://www.amazon.com/BestOffice-Ergonomic-Leather-Office-Chair/dp/B004ZVHKJE%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB004ZVHKJE',
+    category: 'Santa'
+  }, {
+    name: 'Big Joe Dorm Chair, Limo Black',
+    price: 47.14,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51h0RgpH6TL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Big-Joe-Dorm-Chair-Black/dp/B008AE0WV0%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB008AE0WV0',
+    category: 'Santa'
+  }, {
+    name: 'Big Joe Dorm Chair, Limo Black',
+    price: 47.14,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51h0RgpH6TL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Big-Joe-Dorm-Chair-Black/dp/B008AE0WV0%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB008AE0WV0',
+    category: 'Santa'
+  }, {
+    name: 'BINGKA Mens Bomber Jacket Softshell Sportswear Lightweight Slim Jacket Coat',
+    price: 35.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41Sgpby5YHL._SL160_.jpg',
+    URL: 'https://www.amazon.com/BINGKA-Bomber-Softshell-Sportswear-Lightweight/dp/B01H8XL5YG%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB01H8XL5YG',
+    category: 'Santa'
+  }]
+}, {
+  username: 'joos'
+  name: 'Joosang',
+  groups: ['HR50'],
+  items: [{
+    name: 'localmode Men\'s Double Zipper Hooded Jacket Turtleneck Fleece Hoodie Coat Grey M',
+    price: 49.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41zZ1IHGN8L._SL160_.jpg',
+    URL: 'https://www.amazon.com/localmode-Double-Zipper-Hooded-Turtleneck/dp/B01ERDHK5S%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB01ERDHK5S',
+    category: 'Santa'
+  }, {
+    name: 'Five Flags Windproof Torch Lighter 5,10,15,20,25,50,100 Pieces! (5)',
+    price: 5.01,
+    imageURL: 'http://ecx.images-amazon.com/images/I/416etmVhJ9L._SL160_.jpg',
+    URL: 'https://www.amazon.com/Flags-Windproof-Torch-Lighter-Pieces/dp/B00IN7CF8M%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00IN7CF8M',
+    category: 'Elf'
+  }, {
+    name: 'ASICS Men\'s GT 1000 4 Running Shoe, Black/Onyx/Black, 14 M US',
+    price: 100,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41qqlcHoVTL._SL160_.jpg',
+    URL: 'https://www.amazon.com/ASICS-Mens-1000-Running-Black/dp/B00OU970V4%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00OU970V4',
+    category: 'Santa'
+  }, {
+    name: 'Pilot Metropolitan Collection Fountain Pen, Black Barrel, Classic Design, Medium Nib, Black Ink (91107)',
+    price: 12.11,
+    imageURL: 'http://ecx.images-amazon.com/images/I/312UIt5PpkL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Pilot-Metropolitan-Collection-Fountain-91107/dp/B009X9Z2FW%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB009X9Z2FW',
+    category: 'Raindeer'
+  }, {
+    name: 'Mackie CR Series CR3 - 3" Creative Reference Multimedia Monitors (Pair)',
+    price: 99.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41lIje7ibdL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Mackie-CR-CR3-Reference-Multimedia/dp/B00KVEIY4E%3Fpsc%3D1%26SubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00KVEIY4E',
+    category: 'Santa'
+  }, {
+    name: 'Edifier R1280T Powered Bookshelf Speakers - 2.0 Active Near Field Monitors - Studio Monitor Speaker - Wooden Enclosure - 42 Watts RMS',
+    price: 99.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/41rasx6mYrL._SL160_.jpg',
+    URL: 'https://www.amazon.com/Edifier-R1280T-Powered-Bookshelf-Speakers/dp/B016P9HJIA%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB016P9HJIA',
+    category: 'Santa'
+  }, {
+    name: 'TOMOKO 87 Key Mechanical Keyboard, Water-Resistant Mechanical Gaming Keyboard with Blue Switch, Anti-ghosting Keys (Black)',
+    price: 59.99,
+    imageURL: 'http://ecx.images-amazon.com/images/I/51dusQBLwlL._SL160_.jpg',
+    URL: 'https://www.amazon.com/TOMOKO-Mechanical-Keyboard-Water-Resistant-Anti-ghosting/dp/B01DBJTZU2%3FSubscriptionId%3DAKIAIPLG2QYEQ7BELB7A%26tag%3Djslee-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB01DBJTZU2',
+    category: 'Santa'
+  }]
+}]
