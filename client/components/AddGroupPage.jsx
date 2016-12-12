a60bb897796262613671a3720e7f8ece0a0dc1e5
Phil Barow <pbarow@gmail.com>
2016-12-11 21:15:58 -0800

    added axios calls on AddUser and AddGroup pages

--- client/components/AddGroupPage.jsx
+++ client/components/AddGroupPage.jsx
@@ -1,17 +1,44 @@
 //AddGroupPage.jsx
 
 import React from 'react';
+import axios from 'axios'
 
 // not sure if need MOBX here,  will hold off
 
 export default class AddGroupPage extends React.Component {
-  
+
+   constructor (props) {
+    super (props);
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick (e) {
+    e.preventDefault();
+    
+    var data = {
+      group: this.refs.group.value
+    };
+
+    axios.post('/api/groups', {
+      params: {
+        data: data
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  };
 
   render() {
-
     return (
       <div>
         <h2>This is the Add Group Page</h2>
+        <h5>Group:</h5><input type="text" name="group" ref="group" size="50"/><br/>
+        <button name="button" onClick={this.onButtonClick.bind(this)}>Add Group</button>
       </div>
       );
   }
--- client/components/AddUserPage.jsx
+++ client/components/AddUserPage.jsx
@@ -1,6 +1,7 @@
 // AddUserPage.jsx
 
 import React from 'react';
+import axios from 'axios'
 
 // not sure if need MOBX here,  will hold off
 
@@ -19,9 +20,18 @@ export default class AddUserPage extends React.Component {
       username: this.refs.newUser.value,
       name: this.refs.username.value
     };
-    /*
-    AJAX call here
-    */
+
+    axios.post('/api/users', {
+      params: {
+        data: data
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   };
 
@@ -29,9 +39,9 @@ export default class AddUserPage extends React.Component {
     return (
       <div>
         <h2>This is the Add User Page</h2>
-      <h5>Name:</h5><input type="text" name="search" ref="newUser" size="50"/><br/>
-      <h5>UserName:</h5><input type="text" name="search" ref="username" size="50"/><br/>
-      <button name="button" onClick={this.onButtonClick.bind(this)}>Add User</button>
+        <h5>Name:</h5><input type="text" name="newUser" ref="newUser" size="50"/><br/>
+        <h5>UserName:</h5><input type="text" name="username" ref="username" size="50"/><br/>
+        <button name="button" onClick={this.onButtonClick.bind(this)}>Add User</button>
       </div>
       );
   }
